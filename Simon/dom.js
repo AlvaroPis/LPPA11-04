46808d3a8da179234609631d4195f6f7023afd1b
Alvaro Pistelli <alvaropistelli@outlook.com>
2023-07-11 20:41:09 -0300

    Actualizacion interfaz

--- Simon/dom.js
+++ Simon/dom.js
@@ -3,6 +3,9 @@ let Secuencia = [];
 let Jugador = [];
 let Comenzar = false;
 let Nivel = 0;
+let Puntuacion = 0;
+let Tiempo = 0;
+
 
 const botonComenzar = document.getElementById("Comenzar");
 botonComenzar.addEventListener("click", ComenzarJuego);
