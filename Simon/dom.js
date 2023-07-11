6c533dcf422b006436a8f57cc0c1507cc63c1930
Alvaro Pistelli <alvaropistelli@outlook.com>
2023-07-11 18:37:58 -0300

    Comienzo de juego simon

---
+++ Simon/dom.js
@@ -0,0 +1,12 @@
+const Colores = ["Rojo","Verde","Amarillo","Azul"];
+let Secuencia = [];
+let Jugador = [];
+let Comenzar = false;
+let Nivel = 0;
+
+const botonComenzar = document.getElementById("Comenzar");
+botonComenzar.addEventListener("click", ComenzarJuego);
+
+function ComenzarJuego(){
+
+}
